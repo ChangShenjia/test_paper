95b574e2829f68581f8e42cacdc397bfaa815a67
ChangShenjia <413618115@qq.com>
2019-07-10 17:25:44 +0800

    初始化提交

--- 01_prepare_datas.py
+++ 01_prepare_datas.py
@@ -4,6 +4,8 @@ from time import time
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.cluster import KMeans
 
 
 # 1. 加载数据
@@ -39,46 +41,63 @@ num_features = [
     "dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate",
     "dst_host_rerror_rate","dst_host_srv_rerror_rate"
 ]
-features = kdd_data_10percent[num_features].astype(float)
-# print(features.describe())
+features_ten = kdd_data_10percent[num_features].astype(float)
+# print(features_ten.describe())
 # 把输出结果种类减少到正常和攻击两种
 labels_ten = kdd_data_10percent['label'].copy()
 labels_ten[labels_ten != 'normal.'] = 'attack.'
 # print(labels_ten.value_counts())
 
 # 3. 特征缩放(数据归一化)
-# features.apply(lambda x: MinMaxScaler().fit_transform(x))
-features_nor = features.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-# print(features_nor.describe())
-f_t = features_nor.isnull().any()
-f_train = features_nor.fillna('0')
-# print(f_train)
+# features_ten.apply(lambda x: MinMaxScaler().fit_transform(x))
+features_ten_nor = features_ten.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+# print(features_ten_nor.describe())
+f_t = features_ten_nor.isnull().any()
+f_t_train = features_ten_nor.fillna('0')
+# print(f_t_train)
 
-# 4. 训练分类器(knn)
+# 4. 训练/使用分类器(knn)
+# 1) 使用10%的数据集进行训练
 clf = KNeighborsClassifier(n_neighbors=5, algorithm='ball_tree', leaf_size=500)
 t0 = time()
-clf.fit(f_train, labels_ten)
+clf.fit(f_t_train, labels_ten)
 tt = time()-t0
 print("Classifier trained in {} seconds".format(round(tt, 3)))
+# 2) 使用corrected数据集对分类器进行测试
 # 读取corrected数据集文件
-# kdd_data_corrected = pd.read_csv(r"corrected.csv", header=None, names=col_names)
+kdd_data_corrected = pd.read_csv("corrected.csv", header=None, names=col_names)
 # print(kdd_data_corrected['label'].value_counts())
 # 把输出结果种类减少到正常和攻击两种
 # kdd_data_corrected['label'][kdd_data_corrected['label'] != 'normal.'] = 'attack.'
-# labels_c = kdd_data_corrected['label'].copy()
-# labels_c[labels_c != 'normal.'] = 'attack.'
+labels_c = kdd_data_corrected['label'].copy()
+labels_c[labels_c != 'normal.'] = 'attack.'
 # print(labels_c.value_counts())
 # 特征缩放(数据归一化)
-# features_c = kdd_data_corrected[num_features].astype(float)
-# features_c_nor = features_c.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-# print(features_c_nor)
-# features_nor_train, features_nor_test, labels_ten_train, labels_ten_test = train_test_split(
-#     features_c,
-#     kdd_data_corrected['label'],
-#     test_size=0.1,
-#     random_state=42)
+features_c = kdd_data_corrected[num_features].astype(float)
+features_c_nor = features_c.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+# print(features_c_nor.describe())
+f_c = features_c_nor.isnull().any()
+f_c_train = features_c_nor.fillna('0')
+# print(f_c_train)
+features_train, features_test, labels_train, labels_test = train_test_split(
+    f_c_train,
+    labels_c,
+    test_size=0.1,
+    random_state=42)
 # 使用分类器和测试数据进行预测
-# t0 = time()
-# pred = clf.predict(features_nor_test)
-# tt = time() - t0
-# print("Predicted in {} seconds".format(round(tt, 3)))
+t0 = time()
+pred = clf.predict(features_test)
+tt = time() - t0
+print("Predicted in {} seconds".format(round(tt, 3)))
+# 使用测试标签计算R平方值。
+acc = accuracy_score(pred, labels_test)
+print("R squared is {}.".format(round(acc, 4)))
+
+# 5. KMeans聚类
+k = 30
+km = KMeans(n_clusters=k)
+t0 = time()
+km.fit(f_t_train)
+tt = time() - t0
+print("Clustered in {} seconds".format(round(tt, 3)))
+print(pd.Series(km.labels_).value_counts())
